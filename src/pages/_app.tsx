a03264c31b12e7c27f949dde30ab2cc3b1d71905
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-18 21:07:59 -0300

    Next configurado com chakra

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/theme.ts'
+import type { AppProps } from 'next/app'
+import { ChakraProvider} from "@chakra-ui/react" 
+import { theme } from '../styles/theme'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider resetCSS={false} theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>   
+  )
+}
+
+export default MyApp
