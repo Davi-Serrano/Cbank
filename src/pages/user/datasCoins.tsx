8079a27e309e08dbfdb66437948820f960ad0827
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-20 11:14:18 -0300

    datasCoins datas positions

--- src/pages/user/datasCoins.tsx
+++ src/pages/user/datasCoins.tsx
@@ -10,11 +10,8 @@ const options = {
 
 };
 const series = [
-  {
-  name: "serie1", data: [31,120,10,45],}
+  {name: "serie1", data: [31,120,10,45],}
 ]
-  
-
 
 
 export default function DataSCoin() {
@@ -24,15 +21,14 @@ export default function DataSCoin() {
       minChildWidth="320px"
       bg="#2C2C2C"
       w="70%"
-      h="400px"
+      h="450px"
       m="auto"
       mt="2em"
       p="2em"
       >
     <Flex
         flexDir="column"
-        align="center"
-        ml="45px"
+        align="flex-start"
         justify="flex-start"
         h="100px"
         >
@@ -41,26 +37,25 @@ export default function DataSCoin() {
               align="center"
               justify="flex-start"
               h="100px"
-              mb="1em"
+              pl="3em"
               >
 
-                  <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?" height='30px' width='25px' /> 
-                  <Text fontSize={25} px="2em">  
-                      <strong> BitCoin: </strong> R$360.000,00
+                  <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?" height='80px' width='80px' /> 
+                  <Text fontSize={25} px="0.2em">  
+                      <strong> Biticoin :</strong> R$360.000,00
                   </Text>
             </Flex>
             
-            <Chart options={options} series={series} type="area" height={250} width={500} />
+            <Chart options={options} series={series} type="area" height={350} width={500} />
       </Flex>
 
-      <Box py="1em" m="auto" ml="15em"> 
-
+      <Box ml="5em" fontSize={25}> 
         <Flex align="center">
           <Text fontWeight="700">Volume :</Text>
           <Text pl="1em">R$:1.000.000</Text>
         </Flex>
           
-        <Flex align="center">
+        <Flex>
           <Text fontWeight="700">Preço :</Text>
           <Text pl="1em">R$:1.000.000</Text>
         </Flex>
