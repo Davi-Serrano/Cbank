cf276cd1c2d0b9ed183f7a4fde72895306b355b8
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-20 11:41:08 -0300

    Chart Styled

--- src/pages/user/datasCoins.tsx
+++ src/pages/user/datasCoins.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react'
 import dynamic from 'next/dynamic'
 
 const Chart = dynamic(()=> import("react-apexcharts"),{
@@ -6,9 +6,50 @@ const Chart = dynamic(()=> import("react-apexcharts"),{
 })
 
 const options = {
+    chart: {
+      toolbar:{
+        show: false,
+      },
+      zoom:{
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[100]
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels:{
+      enabled: false,
+    },
+    tooltip:{
+      enabled: false,
+    },
+    xaxis:{
+      type: "datetime",
+      axiosBorder: {
+          color: "blue"
+      },
+      axisTicks: {
+          color: "blue"
+      },
+      categories: [
+        '2021-03-18T00:00:00.000Z:',
+        '2021-03-19T00:00:00.000Z:',
+        '2021-03-20T00:00:00.000Z:',
+        '2021-03-21T00:00:00.000Z:'
+      ],
+    },
+    fill:{
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+} as any;
 
-
-};
 const series = [
   {name: "serie1", data: [31,120,10,45],}
 ]
@@ -16,7 +57,7 @@ const series = [
 
 export default function DataSCoin() {
   return(
-    <SimpleGrid
+  <SimpleGrid
       justifyItems="flex-start"
       minChildWidth="320px"
       bg="#2C2C2C"
@@ -26,57 +67,57 @@ export default function DataSCoin() {
       mt="2em"
       p="2em"
       >
-    <Flex
-        flexDir="column"
-        align="flex-start"
-        justify="flex-start"
-        h="100px"
-        >
+        <Flex
+            flexDir="column"
+            align="flex-start"
+            justify="flex-start"
+            h="100px"
+            >
 
-            <Flex
-              align="center"
-              justify="flex-start"
-              h="100px"
-              pl="3em"
-              >
+              <Flex
+                align="center"
+                justify="flex-start"
+                h="100px"
+                pl="3em"
+                >
 
                   <img src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?" height='80px' width='80px' /> 
                   <Text fontSize={25} px="0.2em">  
                       <strong> Biticoin :</strong> R$360.000,00
                   </Text>
+              </Flex>
+                
+              <Chart options={options} series={series} type="area" height={350} width={500} />
+        </Flex>
+
+        <Box ml="5em" fontSize={25}> 
+            <Flex align="center">
+              <Text fontWeight="700">Volume :</Text>
+              <Text pl="1em">R$:1.000.000</Text>
             </Flex>
-            
-            <Chart options={options} series={series} type="area" height={350} width={500} />
-      </Flex>
+              
+            <Flex>
+              <Text fontWeight="700">Preço :</Text>
+              <Text pl="1em">R$:1.000.000</Text>
+            </Flex>
+              
+            <Flex align="center">
+              <Text fontWeight="700">Variação :</Text>
+              <Text pl="1em">5%</Text>
+            </Flex>
 
-      <Box ml="5em" fontSize={25}> 
-        <Flex align="center">
-          <Text fontWeight="700">Volume :</Text>
-          <Text pl="1em">R$:1.000.000</Text>
-        </Flex>
-          
-        <Flex>
-          <Text fontWeight="700">Preço :</Text>
-          <Text pl="1em">R$:1.000.000</Text>
-        </Flex>
-          
-        <Flex align="center">
-          <Text fontWeight="700">Variação :</Text>
-          <Text pl="1em">5%</Text>
-        </Flex>
+            <Flex align="center">
+              <Text fontWeight="700">Última Venda  :</Text>
+              <Text pl="1em">R$:1.000.000</Text>
+            </Flex>
 
-        <Flex align="center">
-          <Text fontWeight="700">Última Venda  :</Text>
-          <Text pl="1em">R$:1.000.000</Text>
-        </Flex>
-
-        <Flex align="center">
-          <Text fontWeight="700">Volume :</Text>
-          <Text pl="1em">R$:1.000.000</Text>
-        </Flex>
-      </Box>
+            <Flex align="center">
+              <Text fontWeight="700">Volume :</Text>
+              <Text pl="1em">R$:1.000.000</Text>
+            </Flex>
+        </Box>
 
       
- </SimpleGrid>
+  </SimpleGrid>
   )
 }
