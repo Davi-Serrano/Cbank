5a524b19b18e9d716e79c667c8fb73b689c32aff
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-22 19:54:48 -0300

    Dados da Api coletados no lado cliente

--- src/pages/user/homeBroker.tsx
+++ src/pages/user/homeBroker.tsx
@@ -1,27 +1,25 @@
-import  {Flex } from "@chakra-ui/react"
+import  {Flex, Table, Tbody, Td, Tr, Image } from "@chakra-ui/react"
 import {SearchInput} from "../../components/SearchInput"
 import axios from "axios"
-import {  GetStaticProps } from 'next'
+import { GetStaticProps } from 'next'
+
+
+
 
 interface CoinProps {
-    id: string,
-    symbol: string,
-    name: string,
-    ImageURL: string,
-    current_price: number,
-    price_change_percentage: number,
+    coin: Array []
 }
 
-export default function HomeBroker({coins}: CoinProps){
+export default function HomeBroker({coins} ){
 
     return(
-        <Flex w="80%" bg="#2C2C2C" justify="center" m="auto" mt="5%">
+        <Flex flexDir="column" w="80%" bg="#2C2C2C" align='center' justify="center" m="auto" mt="5%">
             <SearchInput />
              <Table>
                     <Tbody>
                         <Tr>
-                          <Td><Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" />Bitcoin</Td>
-                          <Td>u$35.594,12</Td>
+                          <Td><Image src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579" w="50px" />Bitcoin</Td>
+                          <Td>U$35.594,12</Td>
                           <Td>6.85</Td>
                           <Td>Buy</Td>
                           <Td>Informations</Td>
@@ -34,14 +32,16 @@ export default function HomeBroker({coins}: CoinProps){
 
 }
 
-const getStaticProps: GetStaticProps = async ()=>{
+export const getStaticProps: GetStaticProps = async ()=>{
 
-    const data = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    const response = await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
     
-    const coins  = data.map(coin =>{
+   
+
+    const coins = response.data.map((coin: CoinProps) =>{
         return {
             id: coin.id,
-            name: coin.nome,
+            name: coin.name,
             symbol: coin.symbol.toUpperCase(),
             imageURL: coin.image,
             current_price: new Intl.NumberFormat('en-US', {
@@ -49,14 +49,18 @@ const getStaticProps: GetStaticProps = async ()=>{
               currency: 'USD', 
             }).format(coin.current_price),
             volume: coin.total_volume,
-            price_change_percentage: coin.price_change_percentage,
+
         }
     })
 
+    console.log(coins)
+
     return {
       props:{
-          coins
-      }
+        coins
+      },      
+      revalidate: 60 // 1min
     }
+
 }
 
