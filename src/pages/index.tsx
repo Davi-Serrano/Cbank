fdcf3cf96389f8d44e0eb7e0a680ba51f4a9e204
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-22 09:26:11 -0300

    useEffect removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,8 @@
 import { Button, Flex, Text } from '@chakra-ui/react'
 import { DiGithubBadge } from "react-icons/di"
-import axios, { AxiosPromise } from "axios"
-import { useEffect } from 'react'
-
-
-
-
+import axios from "axios"
 
 export default function Home() {
-
-  useEffect( async function getApi():Promise<void>{
-     await axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-      .then(res =>console.log(res))
-    
-    
-  }, [])
-  
-  
-  
    return (
     <Flex
       flexDir="column"
