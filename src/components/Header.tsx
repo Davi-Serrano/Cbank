62ac0e4b2a89b57619f6e916c2cdc37af2f8855c
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-27 15:18:32 -0300

    Header and singOut

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,52 @@
-import { Flex , Text} from "@chakra-ui/react";
-import { BsFillPersonFill } from  "react-icons/bs"
+import { Flex ,Button, Image,Text} from "@chakra-ui/react";
+import { useSession, signOut } from "next-auth/react";
 import {Logo} from "./Logo" 
+import { SingInButton } from "./SingInButton";
 
 export  function Header() {
-    const logged = false
-    
+    const {data: session}: any = useSession()
     return(
 
         <Flex
           justify="space-between"
           align="center"
-          p="0 10em"
+          p="1em 10em"
           bg="#032233"
           >
             <Logo />   
-            {logged ?
-                 <p >Login</p> :  
+            {session ?
+                 <Flex
+                 align="center"
+                 justify="space-around"
+                 fontSize={25}
+               >
+                   <Image src={session?.user?.image} w="50px" borderRadius="100%" />
+                   <Text px="0.5em"> {session?.user?.name}</Text>
+                   <Button
+                     bg="#000000"
+                     color="#ffffff"
+                     border="none"
+                     borderRadius="10px"
+                     m="auto"
+                     px="2em"
+                     onClick={()=> signOut()} 
+                     _hover={{
+                             cursor:"pointer",
+                             bg:"#999999"
+                         }
+                      }
+                     >
+                      signOut
+                   </Button>
+               </Flex>
+                :  
                 <Flex
-                  align="center"
-                  justify="space-around"
-                  fontSize={25}
-                >
-                    <BsFillPersonFill/>
-                    <Text px="0.5em">  Davi</Text>
+                 align="center"
+                 justify="space-around"
+                 fontSize={25}
+               >
+
+               <SingInButton />
                 </Flex>
             }
         </Flex> 
--- src/components/SingInButton.tsx
+++ src/components/SingInButton.tsx
@@ -5,17 +5,21 @@ import { DiGithubBadge } from "react-icons/di";
 export function SingInButton(){
     const {data: session} = useSession()
 
-    console.log(session)
-
     return(
     
-        <Button
-            m="auto"
-            bg="#000000"
-            color="#999999"
-            border="none"
-            px="2em"
-            onClick={()=> signIn()} 
+        <Button 
+        bg="#000000"
+        color="#ffffff"
+        border="none"
+        borderRadius="10px"
+        m="auto"
+        px="2em"
+        onClick={()=> signIn()} 
+        _hover={{
+                cursor:"pointer",
+                bg:"#999999"
+            }
+        }
         >
             <DiGithubBadge size={30}/> 
             <Text pl="1em"> 
