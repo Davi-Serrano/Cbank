f0ab73422f5042c41ac57890cea260b386d91398
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-30 09:43:47 -0300

    Try save coins on faunadb

---
+++ src/pages/api/auth/subscribe.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client"
+import { query as q} from "faunadb"
+import { fauna } from "../../../services/fauna";
+
+export default async(req: NextApiRequest, res: NextApiResponse)=> {
+    if (req.method === "POST"){
+        const session = await getSession({req})
+
+        const user = await fauna.query<User>(
+                q.Get(
+                    q.Match(
+                        q.Index('user_by_email'),
+                        q.Casefold(session.user.email)
+                    )
+                )
+            )
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data:{
+                            coin_id: "bitcoin",
+                        }
+                    })
+                )
+    } 
+}
