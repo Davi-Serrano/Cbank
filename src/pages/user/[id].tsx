9f07c90cc0a05276eccb2f5c655c333f6c8d6dd7
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-31 21:02:25 -0300

    errors for id params in user datasCoin

--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -70,100 +70,113 @@ interface CoinProps{
 
 export default  function DataSCoin({coin}: CoinProps) {
 
-  return(
-  <SimpleGrid
-      justifyItems="flex-start"
-      minChildWidth="320px"
-      bg="#2C2C2C"
-      w="70%"
-      h="450px"
-      m="auto"
-      mt="2em"
-      p="2em"
-      >
-        <Flex
-            flexDir="column"
-            align="flex-start"
-            justify="flex-start"
-            h="100px"
-            >
-
-              <Flex
-                align="center"
+  if(coin){
+      return(
+      <SimpleGrid
+          justifyItems="flex-start"
+          minChildWidth="320px"
+          bg="#2C2C2C"
+          w="70%"
+          h="450px"
+          m="auto"
+          mt="2em"
+          p="2em"
+          >
+            <Flex
+                flexDir="column"
+                align="flex-start"
                 justify="flex-start"
                 h="100px"
-                pl="3em"
                 >
 
-                  <img src={coin.image} height='80px' width='80px' /> 
-                  <Text fontSize={25} textTransform="capitalize"  px="0.2em">  
-                        <strong> {coin.id} :</strong> {coin.current_price}
-                  </Text>
-              </Flex>
-                
-              {/* <Chart options={options} series={series} type="area" height={350} width={500} /> */}
-        </Flex>
+                  <Flex
+                    align="center"
+                    justify="flex-start"
+                    h="100px"
+                    pl="3em"
+                    >
 
-        <Box ml="5em" mt="1em" fontSize={20}> 
-            <Flex align="center">
-              <Text fontWeight="700">Volume :</Text>
-              <Text pl="1em"> {coin.total_volume}</Text>
-            </Flex>
-              
-            <Flex>
-              <Text fontWeight="700">Preço :</Text>
-              <Text pl="1em">{coin.current_price}</Text>
-            </Flex>
-              
-            <Flex align="center">
-              <Text fontWeight="700">MarketCap:</Text>
-              <Text pl="1em">{coin.market_cap}</Text>
+                      <img src={coin.image} height='80px' width='80px' /> 
+                      <Text fontSize={25} textTransform="capitalize"  px="0.2em">  
+                            <strong> {coin.id} :</strong> {coin.current_price}
+                      </Text>
+                  </Flex>
+                    
+                  {/* <Chart options={options} series={series} type="area" height={350} width={500} /> */}
             </Flex>
 
-            <Flex align="center">
-              <Text fontWeight="700">Cod :</Text>
-              <Text pl="1em">{coin.symbol}</Text>
-            </Flex>
-        </Box>
+            <Box ml="5em" mt="1em" fontSize={20}> 
+                <Flex align="center">
+                  <Text fontWeight="700">Volume :</Text>
+                  <Text pl="1em"> {coin.total_volume}</Text>
+                </Flex>
+                  
+                <Flex>
+                  <Text fontWeight="700">Preço :</Text>
+                  <Text pl="1em">{coin.current_price}</Text>
+                </Flex>
+                  
+                <Flex align="center">
+                  <Text fontWeight="700">MarketCap:</Text>
+                  <Text pl="1em">{coin.market_cap}</Text>
+                </Flex>
 
-      
-  </SimpleGrid>
-  )
-}
+                <Flex align="center">
+                  <Text fontWeight="700">Cod :</Text>
+                  <Text pl="1em">{coin.symbol}</Text>
+                </Flex>
+            </Box>
+
+          
+      </SimpleGrid>
+      )
+  }
+  return(
+    <h1>Página not found</h1>
+    )
+  }
+
 
 export const getServerSideProps: GetServerSideProps = async({req, params})=>{
-        
-  const { id } = params
 
-  const date = new Date();
-  const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const year = date.getFullYear();
-  const today = day + '-' + month + '-' + year;
+  try {
+          
+    const { id } = params
 
-  const { data } = await api.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${today}&localization=false`)
-  
-  const coin =  {
-        id: data.id,
-        symbol: data.symbol.toUpperCase(),
-        image: data.image.small,
-        current_price: new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD', 
-        }).format(data.market_data.current_price.usd),
-        total_volume: new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD', 
-        }).format(data.market_data.total_volume.usd),
-        market_cap: new Intl.NumberFormat('en-US', {
-          style: 'currency',
-          currency: 'USD', 
-        }).format(data.market_data.market_cap.usd),
-  }
-  
-  return{
-    props:{
-      coin
+    const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const today = day + '-' + month + '-' + year;
+
+    const { data } = await api.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${today}&localization=false`)
+    
+    const coin =  {
+          id: data.id,
+          symbol: data.symbol.toUpperCase(),
+          image: data.image.small,
+          current_price: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD', 
+          }).format(data.market_data.current_price.usd),
+          total_volume: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD', 
+          }).format(data.market_data.total_volume.usd),
+          market_cap: new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD', 
+          }).format(data.market_data.market_cap.usd),
     }
-  }
+    
+    return{
+      props:{
+        coin
+      }
+    }
+  } catch {
+      return{
+        props:{}  
+      }
+    }
 }
