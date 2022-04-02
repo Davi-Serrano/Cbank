5e9f0f631d13003a76ac44b228a71856e2d4dc2c
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-02 14:15:44 -0300

    uptade datas on faunadb with all params

--- src/pages/api/auth/subscribe.ts
+++ src/pages/api/auth/subscribe.ts
@@ -7,19 +7,18 @@ export default async(req: NextApiRequest, res: NextApiResponse)=> {
     if (req.method === "POST"){
         const session = await getSession({req})
 
-        // const user = await fauna.query<any>(
-        //         q.Get(
-        //             q.Match(
-        //                 q.Index('user_by_email'),
-        //                 q.Casefold(session.user.id)
-        //             )
-        //         )
-        //     )
-        console.log(req.body)
+        const user = await fauna.query<any>(
+                q.Get(
+                    q.Match(
+                        q.Index('user_by_email'),
+                        q.Casefold(session.user.email)
+                    )
+                )
+            )
 
             await fauna.query(
                 q.Update(
-                  q.Ref(q.Collection('users'), "327505244514680899"),
+                  q.Ref(q.Collection('users'), user.ref.id),
                   {
                       data:{
                           coin_id: [req.body],
