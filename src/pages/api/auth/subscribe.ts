c141b49237b7802aefb32f222f3fa4e2c049549e
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-02 23:39:11 -0300

    verification for save coin on faunadb

--- src/pages/api/auth/subscribe.ts
+++ src/pages/api/auth/subscribe.ts
@@ -2,10 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react"
 import { query as q} from "faunadb"
 import { fauna } from "../../../services/fauna";
+interface CoinsFiltredProps {
+ name: string
+}
+
 
 export default async(req: NextApiRequest, res: NextApiResponse)=> {
     if (req.method === "POST"){
-        const session = await getSession({req})
+        const session: any = await getSession({req})
 
         const user = await fauna.query<any>(
                 q.Get(
@@ -16,20 +20,43 @@ export default async(req: NextApiRequest, res: NextApiResponse)=> {
                 )
             )
 
-            const coinArray = [...user.data.coin_id, req.body]
-
+        if(!user.data.coin_id){
             await fauna.query(
                 q.Update(
-                  q.Ref(q.Collection('users'), user.ref.id),
-                  {
-                      data:{
-                          coin_id: coinArray,
-                      }
-                  }
+                q.Ref(q.Collection('users'), user.ref.id),
+                {
+                    data:{
+                        coin_id: [req.body],
+                    }
+                }
                 )
-              )
+            )
         
-              res.status(200).json("SUCcESS")
+            res.status(200).json("SUCCESS")
+        } else{
+            const COINS_VERIFICATION = user.data.coin_id.filter((coin: CoinsFiltredProps )=>{
+                return coin.name == req.body.name
+            })
+            
 
+            if(COINS_VERIFICATION.length === 0){
+                const coinArray = [...user.data.coin_id, req.body]
+
+                await fauna.query(
+                    q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data:{
+                            coin_id: coinArray,
+                        }
+                    }
+                    )
+                )
+            
+                res.status(200).json("SUCCESS")
+            } 
+        
+        }   
+            
     } 
 }
