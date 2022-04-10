885271165b3d69cd5673a47c57cd77caed5a4743
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-09 22:47:11 -0300

    Chart add in coin Informations again

--- src/pages/user/coin/[id].tsx
+++ src/pages/user/coin/[id].tsx
@@ -1,9 +1,11 @@
-// import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic'
 import { GetServerSideProps } from "next"
 import { api } from '../../../services/api';
 
-import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react'
 import { getSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 interface CoinProps{
   coin:{
@@ -16,62 +18,96 @@ interface CoinProps{
   }
 }
 
-// const Chart = dynamic(()=> import("react-apexcharts"),{
-//   ssr: false
-// })
+const Chart = dynamic(()=> import("react-apexcharts"),{
+  ssr: false
+})
 
-// const options = {
-//     chart: {
-//       toolbar:{
-//         show: false,
-//       },
-//       zoom:{
-//         enabled: false,
-//       },
-//       foreColor: theme.colors.gray[100]
-//     },
-//     grid: {
-//       show: false,
-//     },
-//     dataLabels:{
-//       enabled: false,
-//     },
-//     tooltip:{
-//       enabled: false,
-//     },
-//     xaxis:{
-//       type: "datetime",
-//       axiosBorder: {
-//           color: "blue"
-//       },
-//       axisTicks: {
-//           color: "blue"
-//       },
-//       categories: [
-//         '2021-03-18T00:00:00.000Z:',
-//         '2021-03-19T00:00:00.000Z:',
-//         '2021-03-20T00:00:00.000Z:',
-//         '2021-03-21T00:00:00.000Z:'
-//       ],
-//     },
-//     fill:{
-//       opacity: 0.3,
-//       type: "gradient",
-//       gradient: {
-//         shade: "dark",
-//         opacityFrom: 0.7,
-//         opacityTo: 0.3,
-//       },
-//     },
-// } as any;
+const options = {
+    chart: {
+      toolbar:{
+        show: false,
+      },
+      zoom:{
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[100]
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels:{
+      enabled: false,
+    },
+    tooltip:{
+      enabled: false,
+    },
+    xaxis:{
+      type: "datetime",
+      axiosBorder: {
+          color: "blue"
+      },
+      axisTicks: {
+          color: "blue"
+      },
+      categories: [
+        '2021-03-18T00:00:00.000Z:',
+        '2021-03-19T00:00:00.000Z:',
+        '2021-03-20T00:00:00.000Z:',
+        '2021-03-21T00:00:00.000Z:'
+      ],
+    },
+    fill:{
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+} as any;
 
-// const series = [
-//   {name: "serie1", data: [31,120,10,45],}
-// ]
+const series = [
+  {name: "serie1", data: [31,120,10,45],}
+]
 
-export default  function DataSCoin({coin}: CoinProps) {
+export default  function DataSCoin() {
+  const date = new Date();
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    const today = day + '-' + month + '-' + year;
 
-  if(coin){
+  const router = useRouter()
+  const id = router.query.id
+
+  console.log(id)
+
+  const [ state, setState] = useState({
+    id: "null",
+    symbol: "null",
+    image:{
+      small: "null"
+    },
+    market_data:{
+      current_price:{
+        usd: 432,
+      },
+      total_volume: {
+        usd: 432,
+      },
+      market_cap:{
+        usd: 432,
+      }
+    },
+
+  })
+
+  useEffect (async ()=>{
+    const { data } = await api.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${today}&localization=false`)
+    setState(data)
+  }, [])
+
       return(
       <SimpleGrid
           justifyItems="flex-start"
@@ -97,34 +133,34 @@ export default  function DataSCoin({coin}: CoinProps) {
                     pl="3em"
                     >
 
-                      <img src={coin.image} height='80px' width='80px' /> 
+                      <img src={state.image.small} height='80px' width='80px' /> 
                       <Text fontSize={25} textTransform="capitalize"  px="0.2em">  
-                            <strong> {coin.id} :</strong> {coin.current_price}
+                            <strong> {state.id} :</strong> U${state.market_data.current_price.usd.toFixed(2)}
                       </Text>
                   </Flex>
                     
-                  {/* <Chart options={options} series={series} type="area" height={350} width={500} /> */}
+                  <Chart options={options} series={series} type="area" height={350} width={500} />
             </Flex>
 
             <Box ml="5em" mt="1em" fontSize={20}> 
                 <Flex align="center">
                   <Text fontWeight="700">Volume :</Text>
-                  <Text pl="1em"> {coin.total_volume}</Text>
+                  <Text pl="0.2em"> U$ {state.market_data.total_volume.usd.toFixed(2)}</Text>
                 </Flex>
                   
                 <Flex>
                   <Text fontWeight="700">Preço :</Text>
-                  <Text pl="1em">{coin.current_price}</Text>
+                  <Text pl="0.2em">U$ {state.market_data.current_price.usd.toFixed(2)}</Text>
                 </Flex>
                   
                 <Flex align="center">
                   <Text fontWeight="700">MarketCap:</Text>
-                  <Text pl="1em">{coin.market_cap}</Text>
+                  <Text pl="0.2em">U$ {state.market_data.market_cap.usd.toFixed(2)}</Text>
                 </Flex>
 
                 <Flex align="center">
                   <Text fontWeight="700">Cod :</Text>
-                  <Text pl="1em">{coin.symbol}</Text>
+                  <Text pl="1em">{state.symbol.toUpperCase()}</Text>
                 </Flex>
             </Box>
 
@@ -132,62 +168,60 @@ export default  function DataSCoin({coin}: CoinProps) {
       </SimpleGrid>
       )
   }
-  return(
-    <h1>Página not found</h1>
-    )
-  }
 
 
-export const getServerSideProps: GetServerSideProps = async({req, params})=>{
-  const session = await getSession({req})
+// export const getServerSideProps: GetServerSideProps = async({req, params})=>{
+//   const session = await getSession({req})
 
-  if(!session){
-    return{
-      redirect:{
-        destination: "/",
-        permanent: false
-      }
-    }
-  }
+//   if(!session){
+//     return{
+//       redirect:{
+//         destination: "/",
+//         permanent: false
+//       }
+//     }
+//   }
 
-  try {
+//   try {
           
-    const { id } = params
+//     const { id } = params
 
-    const date = new Date();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    const today = day + '-' + month + '-' + year;
+//     const date = new Date();
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const month = String(date.getMonth() + 1).padStart(2, '0');
+//     const year = date.getFullYear();
+//     const today = day + '-' + month + '-' + year;
 
-    const { data } = await api.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${today}&localization=false`)
+//     const { data } = await api.get(`https://api.coingecko.com/api/v3/coins/${id}/history?date=${today}&localization=false`)
+//     console.log(id)
+//     console.log()
     
-    const coin =  {
-          id: data.id,
-          symbol: data.symbol.toUpperCase(),
-          image: data.image.small,
-          current_price: new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD', 
-          }).format(data.market_data.current_price.usd),
-          total_volume: new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD', 
-          }).format(data.market_data.total_volume.usd),
-          market_cap: new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD', 
-          }).format(data.market_data.market_cap.usd),
-    }
+//     const coin =  {
+//           id: data.id,
+//           symbol: data.symbol.toUpperCase(),
+//           image: data.image.small,
+//           current_price: new Intl.NumberFormat('en-US', {
+//             style: 'currency',
+//             currency: 'USD', 
+//           }).format(data.market_data.current_price.usd),
+//           total_volume: new Intl.NumberFormat('en-US', {
+//             style: 'currency',
+//             currency: 'USD', 
+//           }).format(data.market_data.total_volume.usd),
+//           market_cap: new Intl.NumberFormat('en-US', {
+//             style: 'currency',
+//             currency: 'USD', 
+//           }).format(data.market_data.market_cap.usd),
+//     }
     
-    return{
-      props:{
-        coin
-      }
-    }
-  } catch {
-      return{
-        props:{}  
-      }
-    }
-}
+//     return{
+//       props:{
+//         coin
+//       }
+//     }
+//   } catch {
+//       return{
+//         props:{}  
+//       }
+//     }
+// }
