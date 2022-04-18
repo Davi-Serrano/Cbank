190794771d243e71d95a98eaa30318782ebe7d13
Davi Serrano <daviscardoso2901@gmail.com>
2022-04-18 13:50:32 -0300

    Refactoring delete

--- src/pages/api/delete.ts
+++ src/pages/api/delete.ts
@@ -25,64 +25,74 @@ interface User{
 };
 
 export default async(req: NextApiRequest, res: NextApiResponse)=> {
+    //Verify if method is equal POST.
     if(req.method === "POST"){
-        const session: SessionProps | any = await getSession({req})
-        const  {name}  = req.body;
+        const session: SessionProps | any = await getSession({req}); ///Get the user data of session.
+        const { name }  = req.body; //Get the coin name of the body.
 
+        //Get user data in faunaDB.
         const user = await fauna.query<User>(
-            q.Get(
-                q.Match(
-                    q.Index('user_by_email'),
-                    q.Casefold(session.user.email)
-                )
-            )
-        );
+                                q.Get(
+                                    q.Match(
+                                        q.Index('user_by_email'),
+                                        q.Casefold(session.user.email)
+                                    )
+                                )
+                            );
 
-        const coins = user.data.coin_id;
+        //All coins in faunaDB.
+        const coins = user.data.coin_id;  
 
+         //Find the index in array coins.
         const coinIndex = coins.findIndex( coin => coin.name === name);
-
+        
+        //Verify if coin exists on array.
         if(coinIndex === -1){
-            console.log("Coin not found")
-        }else{
 
-            if(coins[coinIndex].quantify === 1){
-                    coins.splice(coinIndex, 1);
-                        
-                    const coinArray = [...coins]
-                    
-                    await fauna.query(
-                        q.Update(
-                            q.Ref(q.Collection('users'), user.ref.id),
-                            {
-                                data:{
-                                    coin_id: coinArray,
-                                }
+            return res.status(400).json({error: "Coin not found"});   
+        //Verify if is the last coin on bank and delete.
+        }else if(coins[coinIndex].quantify === 1){
+            //Delete coin.
+            coins.splice(coinIndex, 1);
+
+            //Update the array without the coin.
+            const coinArrayUpdated = [...coins] 
+
+            //Save coins updated on faunaDB.
+            await fauna.query(
+                    q.Update(
+                        q.Ref(q.Collection('users'), user.ref.id),
+                        {
+                            data:{
+                                coin_id: coinArrayUpdated,
                             }
-                        )
-                    );
-                    
-                    return res.status(200).json("Coin deleted with success");
-            } else {
-                const coinArray = user.data.coin_id
+                        }
+                    )
+                );
+                
+            return res.status(200).json("Coin deleted with success");
+        } else {
+                const coinArrayUpdated = user.data.coin_id; // Get the array of coins. 
 
-                Object.assign(coinArray[coinIndex], {
+                //Updated quantify of coins in the bank.
+                Object.assign(coinArrayUpdated[coinIndex], {
                     quantify: user.data.coin_id[coinIndex].quantify - 1
                 })
 
+                //Updated the coins datas in faunaDB.
                 await fauna.query(
-                    q.Update(
-                        q.Ref(q.Collection('users'), user.ref.id),
-                            {
-                                data:{
-                                    coin_id: coinArray,
-                                }
-                            }
-                    )
-                );
+                            q.Update(
+                                q.Ref(q.Collection('users'), user.ref.id),
+                                    {
+                                        data:{
+                                            coin_id: coinArrayUpdated,
+                                        }
+                                    }
+                            )
+                        );
 
                 return res.status(200).json("Coin updated")
             }
-        }
    }
 }
+
